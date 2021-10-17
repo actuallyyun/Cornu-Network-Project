14ba5d8eb9a1c763558686f2aa29b57c9ad51a0b
Yun <irene.jiyun@gmail.com>
2021-10-17 16:20:01 +0200

    Created userfollowing model

--- network/models.py
+++ network/models.py
@@ -1,6 +1,34 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models.fields.related import ForeignKey
+from django.utils import timezone
 
 
 class User(AbstractUser):
     pass
+
+    def follows(self):
+        return [u.following_user_id.username for u in self.following.all()]
+
+    def followed_by(self):
+        return [f.user_id.username for f in self.followers.all()]
+
+
+class UserFollowing(models.Model):
+    user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="following")
+    following_user_id = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="followers")
+
+# A user should have a followers filed and a follows field.
+# These fields should be linked to User model using ForeignKeys
+
+
+# class Post(models.Model):
+#     poster = models.ForeignKey(User, on_delete=models.CASCADE)
+# # TODO Other arguments to add to the poster field
+
+# # TODO should I use textField or CharField in this case?
+#     content = models.TextField(on_delete=models.CASCADE)
+#     date_time_created = models.DateTimeField(default=timezone.now)
+#     likes = models.IntegerField(default=0)
