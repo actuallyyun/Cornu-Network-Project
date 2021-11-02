3d03de1502d7028bdd41730bdf3379a7355d8da3
Yun <irene.jiyun@gmail.com>
2021-11-02 17:40:49 +0100

    "added some def functions"# Please enter the commit message for your changes. Lines starting

--- network/models.py
+++ network/models.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.fields.related import ForeignKey
@@ -8,26 +9,66 @@ class User(AbstractUser):
     pass
 
     def follows(self):
-        return [u.following_user_id.username for u in self.following.all()]
+        return [u.following_user.username for u in self.following.all()]
 
     def followed_by(self):
-        return [f.user_id.username for f in self.followers.all()]
+        return [f.user.username for f in self.followers.all()]
+
+    def follows_user_object(self):
+        return [u.following_user for u in self.following.all()]
+
+    def posts_content(self):
+        return [p.content for p in self.posts.all()]
+
+    def num_follows(self):
+        return self.following.all().count()
+
+    def num_followers(self):
+        return self.followers.all().count()
+
+    def posts_followed_user(self):
+
+        posts = []
+        for u in self.follows_user_object():
+            post_queryset = Post.objects.filter(poster=u).order_by(
+                "-date_time_created")
+            for p in post_queryset:
+                posts.append(p)
+
+        return posts
 
 
 class UserFollowing(models.Model):
-    user_id = models.ForeignKey(
+    user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="following")
-    following_user_id = models.ForeignKey(
+    following_user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="followers")
 
-# A user should have a followers filed and a follows field.
-# These fields should be linked to User model using ForeignKeys
+    # TODO A user shouldn't be able to follow herself
+
+    def __str__(self):
+        return f'{self.user}'
 
 
 class Post(models.Model):
-    poster = models.ForeignKey(User, on_delete=models.CASCADE)
-# TODO Other arguments to add to the poster field
+    poster = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="posts")
+    # TODO Other arguments to add to the poster field
 
     content = models.TextField()
     date_time_created = models.DateTimeField(default=timezone.now)
     likes = models.IntegerField(default=0)
+
+    def serialize(self):
+        return {
+            "poster": self.poster.username,
+            "content": self.content,
+            "timestamp": self.date_time_created.strftime("%b %d %Y, %I:%M %p"),
+            "likes": self.likes
+        }
+
+    def time_posted(self):
+        return self.date_time_created.strftime("%b %d")
+
+    def poster_id(self):
+        return User.objects.get(username=self.poster).id
